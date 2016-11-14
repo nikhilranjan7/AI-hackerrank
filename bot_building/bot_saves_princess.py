4517a463c1ed87767bd2c7a674ad2acab3bcf02f
nikhilranjan7 <nikhilranjan7@gmail.com>
2016-11-15 00:44:43 +0530

    Solve Bot Saves Princess

---
+++ bot_building/bot_saves_princess.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+def printa(s):
+    for i in range(0,m/2):
+        print(s)
+
+def displayPathtoPrincess(n,grid):
+    #print all the moves here
+    if 'p' in grid[-1]:
+            printa('DOWN')
+        if (grid[-1].index('p'))!=0:
+            printa('RIGHT')
+        else:
+            printa('LE#!/usr/bin/python
+def printa(s):
+    for i in range(0,m//2):
+        print(s)
+
+def displayPathtoPrincess(n,grid):
+    #print all the moves here
+    if 'p' in grid[-1]:
+        printa('DOWN')
+        if (grid[-1].index('p'))!=0:
+            printa('RIGHT')
+        else:
+            printa('LEFT')
+    else:
+        printa('UP')
+        if (grid[0].index('p'))!=0:
+            printa('RIGHT')
+        else:
+            printa('LEFT')
+
+m = int(input())
+grid = []
+for i in range(0, m):
+    grid.append(input().strip())
+
+displayPathtoPrincess(m,grid)
+
+FT')
+    else:
+        print('UP')
+        if (grid[0].index('p'))!=0:
+            printa('RIGHT')
+        else:
+            printa('LEFT')
+
+m = int(input())
+grid = []
+for i in range(0, m):
+    grid.append(input().strip())
+
+displayPathtoPrincess(m,grid)
